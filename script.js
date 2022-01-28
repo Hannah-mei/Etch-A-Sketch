e5f92ca72227d8d1c6b372cd3f3c9b9c384b4b35
Hanna <hannaiakovlieva@gmail.com>
2022-01-28 18:49:14 +0100

    Added changable size to the grid

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+let side=Number(prompt('How many cells per side would you like?', ''));
+let cellAmount=side ** 2;
+let cellSize=600/side;
+
 const container=document.getElementById('container');
 
-for (let i=0; i<256; i++) {
+for (let i=0; i<cellAmount; i++) {
     const cell=document.createElement('div');
     container.appendChild(cell)
-
+    cell.style.height=`${cellSize}px`;
+    cell.style.width=`${cellSize}px`;
 }
 
